c0c93e3a4bf49b3b26fc2c9e029bfc62a53823d7
David Shittabey <101060511+Kvng-Divad@users.noreply.github.com>
2022-08-05 11:31:37 +0100

    update

--- js/script.js
+++ js/script.js
@@ -27,3 +27,59 @@ else{
     item.classList.add('accordion-open')
 }
 }
+
+
+/* swiper */
+var swiperReview = new Swiper (".review-slider", {
+    spaceBetween: 20,
+    loop:true,
+    grabCursor: true,
+    autoplay:{
+        delay: 7500,
+        disableOnInteraction:false,
+    },
+    breakpoints:{
+        0: {
+            slidesPerView: '1',
+        },
+        600: {
+            slidesPerView: '2',
+        },
+    },
+    centeredSlides:false,
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    pagination:{
+        el:'.swiper-pagination',
+        clickable:true,
+    },  
+});
+
+
+
+
+/*--------Dark/ Light Theme-------
+let dayNight = document.querySelector("#theme-button");
+dayNight.addEventListener('click', () => {
+    dayNight.classList.toggle('fa-sun')
+    dayNight.classList.toggle('fa-moon')
+    document.body.classList.toggle('dark');
+});
+
+
+/*--------scroll- Reveal-------
+const sr = ScrollReveal ({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay:400,
+    //reset:true
+})
+
+sr.reveal('.home, .about, .dishes, .menu, .footer-container, .review, .order,.fast-food',)
+sr.reveal('.dishes-box, .box', {interval:50, reset:true})
+sr.reveal('.about-image', {origin: 'right'})
+sr.reveal('.about-content, .order, ', {origin: 'left'})*/
